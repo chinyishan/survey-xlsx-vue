be2f87fac367d9a18081b8545cabc9b3ed4593b5
sunny <star138020@gmail.com>
2024-12-06 09:46:19 +0800

    新增上傳檔案下載

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import AutoImport from 'unplugin-auto-import/vite'; //自動導入
+import Components from 'unplugin-vue-components/vite'; //自動導入
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'; //自動導入
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
